89173c6f80c1bd424a806acf8fedeab58e31e5e8
Muhammed Jinas <57747927+JinasMuhammed@users.noreply.github.com>
2020-01-06 13:16:44 +0530

    order model added

---
+++ model/order.model.js
@@ -0,0 +1,20 @@
+//db schema
+const mongoose =require('mongoose');
+const schema = mongoose.schema;
+
+//declaring collection schema property and names 
+let order = new schema(
+    {
+        //schema property:values
+        username:{type:String},
+        address:{type:String},
+        mobile:{type:String},
+        orderinfo:{type:String},
+        orderstatus:{type:String},
+        orderdate:{type:String}
+    },
+    {
+        collection:'order'
+    }
+);
+module.exports= mongoose.model('Order',Order)
